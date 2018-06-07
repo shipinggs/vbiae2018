3f9827355c3ec4c9d1915e2cb28396beb41c7e90
Shi Ping <shiping.lim@outlook.com>
2018-06-07 14:39:55 +0800

    Resized images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,6 @@ import { Meta } from '@angular/platform-browser';
 export class AppComponent {
   title = 'No More Free Space?';  
   constructor(private meta: Meta) {
-    this.meta.addTag({ name: "description", content: "The Singapore Pavilion's 'No More Free Space?' exhibition features creative use of space in land-scarce Singapore." });
+    this.meta.addTag({ name: "description", content: "The Singapore Pavilion's 'No More Free Space?' exhibition at the Venice Biennale 2018 features creative use of space in land-scarce Singapore." });
   }
 }
